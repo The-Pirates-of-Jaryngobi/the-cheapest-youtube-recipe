9e61dd05e96c6c50ecfcf63e4572d8c6299108ce
Jaeho-99 <wogh6860@gmail.com>
2024-02-22 13:22:21 +0900

    feat: 유튜브 크롤링 후 csv 저장 코드

--- ETL_pipelines/youtube/youtube_crawler.py
+++ ETL_pipelines/youtube/youtube_crawler.py
@@ -9,6 +9,8 @@ import os
 import pandas as pd
 from dotenv import load_dotenv
 import google.generativeai as genai
+from datetime import datetime
+import time
 
 class YoutubeCrawler:
     def __init__(self):
@@ -25,7 +27,7 @@ class YoutubeCrawler:
         
     def set_youtube_api(self):
         load_dotenv('../../resource/secret.env')
-        self.GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+        self.GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY1')
         
         os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1" # 보안 통신(SSL/TLS)을 사용하지 않고 HTTP를 통해 OAuth 인증을 수행.
         self.youtube = googleapiclient.discovery.build(
@@ -42,8 +44,7 @@ class YoutubeCrawler:
             df = pd.read_csv(file_path)
             cook_name_list = df["식품명"].tolist()
             
-            print(f"총 요리명 수: {len(cook_name_list)}")
-            print("*"*80)
+            print(f"총 요리 수: {len(cook_name_list)}")
             
             return cook_name_list
             
@@ -61,15 +62,12 @@ class YoutubeCrawler:
             )
             response = request.execute()
             
-            print(f"<{cook_name} 레시피에 대한 검색 결과>")
+            print(f"<{cook_name} 레시피에 대한 검색 완료>")
             video_url_list = []
             for item in response["items"]:
                 youtube_video_id = item["id"]["videoId"]
                 video_url = "https://www.youtube.com/watch?v="+youtube_video_id
                 video_url_list.append(video_url)
-            
-            print(f"총 동영상 수 : {len(video_url_list)}")
-            print("*"*80)
             
             return video_url_list
             
@@ -82,7 +80,7 @@ class YoutubeCrawler:
             print(f"{video_url}에 대한 동영상 정보를 추출합니다.")
             
             self.driver.get(video_url)
-            self.driver.implicitly_wait(60)
+            self.driver.implicitly_wait(10)
             
             # 영상 정보 더보기 버튼 클릭.
             expand_btn_element = self.driver.find_element(By.ID, "expand")
@@ -91,52 +89,33 @@ class YoutubeCrawler:
             
             thumbnail_element = self.driver.find_element(By.XPATH, "/html/body/div[1]/link[2]")
             thumbnail = thumbnail_element.get_attribute("href")
-            print(f"thumbnail : {thumbnail}")
-            print("-"*60)
             
             title_element = self.driver.find_element(By.XPATH, "/html/head/meta[3]")
             title = title_element.get_attribute("content")
-            print(f"title : {title}")
-            print("-"*60)
             
             channel_img_element = self.driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-watch-metadata/div/div[2]/div[1]/ytd-video-owner-renderer/a/yt-img-shadow/img")
             channel_img = channel_img_element.get_attribute("src")
-            print(f"channel_img : {channel_img}")
-            print("-"*60)
             
             channel_name_element = self.driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-watch-metadata/div/div[2]/div[1]/ytd-video-owner-renderer/div[1]/ytd-channel-name/div/div/yt-formatted-string/a")
             channel_name = channel_name_element.text
-            print(f"channel_name : {channel_name}")
-            print("-"*60)
+            channel_id = channel_name_element.get_attribute("href")
             
             subscribers_count_element = self.driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-watch-metadata/div/div[2]/div[1]/ytd-video-owner-renderer/div[1]/yt-formatted-string")
             subscribers_count = subscribers_count_element.text
-            print(f"subscribers_count : {subscribers_count}")
-            print("-"*60)
             
             thumbsup_count_element = self.driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-watch-metadata/div/div[2]/div[2]/div/div/ytd-menu-renderer/div[1]/segmented-like-dislike-button-view-model/yt-smartimation/div/div/like-button-view-model/toggle-button-view-model/button-view-model/button/div[2]")
             thumbsup_count = thumbsup_count_element.text
-            print(f"thumbsup_count : {thumbsup_count}")
-            print("-"*60)
             
             views_count_element = self.driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-watch-metadata/div/div[4]/div[1]/div/ytd-watch-info-text/div/yt-formatted-string/span[1]")
             views_count = views_count_element.text
-            print(f"views_count : {views_count}")
-            print("-"*60)
             
             uploaded_date_element = self.driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-watch-metadata/div/div[4]/div[1]/div/ytd-watch-info-text/div/yt-formatted-string/span[3]")
             uploaded_date = uploaded_date_element.text
-            print(f"uploaded_date : {uploaded_date}")
-            print("-"*60)
             
             text_element = self.driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-watch-metadata/div/div[4]/div[1]/div/ytd-text-inline-expander/yt-attributed-string")
             text = text_element.text
-            print(f"text : {text}")
-            print("-"*60)
             
-            print("*"*80)
-            
-            return [thumbnail, title, channel_img, channel_name, subscribers_count, thumbsup_count, views_count, uploaded_date, text]
+            return (thumbnail, title, channel_img, channel_name, channel_id, subscribers_count, thumbsup_count, views_count, uploaded_date, text)
             
         except Exception as e:
             print(f"에러 발생 >> {e}")
@@ -152,21 +131,41 @@ if __name__ == "__main__":
     youtubeCrawler.set_webdriver()
     youtubeCrawler.set_youtube_api()
     youtubeCrawler.set_gemini_api()
-        
+    
     cook_name_file_path = "cook_basic.csv"
     cook_name_list = youtubeCrawler.get_cook_name_list(cook_name_file_path)
     
-    for i in range(3):
+    column_list = ['cook_name', 'video_id', 'video_title', 'video_thumbnail', 'video_thumbsup_count', 'video_views_count', 'video_uploaded_date', 'video_text', 'channel_id', 'channel_name', 'channel_img']
+    row_list = []
+    
+    for i in range(len(cook_name_list)):
+        print('*'*80)
+        print('cook_name_list index:', i)
         cook_name = cook_name_list[i]
-        video_url_list = youtubeCrawler.search_cook_name(cook_name)
-        for j in range(3):
+        try:
+            video_url_list = youtubeCrawler.search_cook_name(cook_name)
+        except:
+            break
+        for j in range(len(video_url_list)):
+            print('video_url_list index:', j)
             video_url = video_url_list[j]
-            video_infos = youtubeCrawler.get_video_infos(video_url)
-            query_string = video_infos[-1] + '\n 위 정보에서 "(재료명1, 첨가량1)\n(재료명2, 첨가량2)\n(재료명3, 첨가량3)" 형식으로 정보 리스트 내용만 반환해줘.'
-            print(query_string)
-            gemini_return_values = youtubeCrawler.query_to_gemini(video_infos[-1])
-            print('-'*100)
-            print('gemini 호출 결과 : ', gemini_return_values)
-            print('-'*100)
-            
+            try:
+                (thumbnail, title, channel_img, channel_name, channel_id, subscribers_count, thumbsup_count, views_count, uploaded_date, text) = youtubeCrawler.get_video_infos(video_url)
+            except:
+                youtubeCrawler.set_webdriver()
+                continue
+            time.sleep(0.01)
+            row = [cook_name, video_url, title, thumbnail, thumbsup_count, views_count, uploaded_date, text, channel_id, channel_name, channel_img]
+            row_list.append(row)
+    
+    df = pd.DataFrame(row_list, columns=column_list)
+
+    # CSV 파일 이름 설정
+    csv_filename = f'youtube_raw_dataset_{datetime.now().strftime("%Y-%m-%d-%H-%M")}.csv'
+
+    # DataFrame을 CSV 파일로 저장
+    df.to_csv(csv_filename, index=False)
+
+    print(f"파일 '{csv_filename}'이 생성되었습니다.")
+    
     youtubeCrawler.quit_webdriver()
